9b1131529ec46ee10d5aeb3ad416094c557a4ec3
michelle bui <mich@dhcp-v008-150.mobile.uci.edu>
2023-11-04 14:24:41 -0700

    fewfe

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,9 @@ import Footer from '@components/Footer'
 export default function Home() {
   return (
     <div className="container">
-      <Head>
-        <title>Next.js Starter!</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
 
       <main>
-        <Header title="Welcome to Michelle's app!" />
+        <Header title="we need to talk about pcos" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
