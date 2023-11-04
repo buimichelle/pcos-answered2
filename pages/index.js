88438e42579b46b2c1284d5bc2eab33ed6aefc8a
michelle bui <mich@dhcp-v008-150.mobile.uci.edu>
2023-11-04 14:31:49 -0700

    added a picture

--- pages/index.js
+++ pages/index.js
@@ -8,9 +8,6 @@ export default function Home() {
 
       <main>
         <Header title="we need to talk about pcos" />
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
       </main>
 
       <Footer />
