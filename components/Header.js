564054ce602d78cf2dee234779fd88adb4b7718c
michelle bui <mich@michelles-MacBook-Pro.local>
2023-11-10 13:07:17 -0800

    fixed the icon

--- components/Header.js
+++ components/Header.js
@@ -3,11 +3,13 @@ import Link from 'next/link';
 export default function Header() {
   return (
     <div>
-      <h1 className="header">
-        <Link href="/landing">
-          <a>PCOS ANSWERED</a>
-        </Link>
-      </h1>
+      <body>
+        <h1 className="header">
+          <Link href="/landing">
+            <a>PCOS ANSWERED</a>
+          </Link>
+        </h1>
+      </body>
     </div>
 
   );
--- pages/index.js
+++ pages/index.js
@@ -5,19 +5,50 @@ import Footer from '@components/Footer';
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
+        <link rel="icon" href="ribbon.png" type="image/x-icon" />
+        <link rel="shortcut icon" href="ribbon.png" type="image/x-icon" />
         <title>pcos answered</title>
         <meta name="description" content="spreading awareness about pcos!" />
       </Head>
+
+
       <Header />
-      <main>
-        <h1 className="title">we really need to <br />talk about PCOS</h1>
-        <Link href="/landing">
-          <a className="styled-button">OKAY</a>
-        </Link>
-      </main>
-    </div>
+      <div class="container">
+        <img src="beige-women.jpg" alt="<a href='https://www.freepik.com/free-photo/lot-diverse-women-tops-pants-are-huddled-together-looking-posing-camera-against-beige-background_26258358.htm#query=women&position=18&from_view=search&track=sph'>Image by lookstudio</a> on Freepik"></img>
+        <div class="centered">
+          <p>Worldwide, PCOS affects 6% to 10% of women, making it the most common endocrine disorder in women of childbearing age.</p>
+          <Link className="button" href="/symptoms">
+            <a className="styled-button">SYMPTOMS</a>
+          </Link>
+          <Link href="/research">
+            <a className="styled-button">RESEARCH</a>
+          </Link>
+          <Link href="/resources">
+            <a className="styled-button">RESOURCES</a>
+          </Link>
+          <Link href="https://www.pcosaa.org/donate" >
+            <a className="styled-button" target="_blank" rel="noopener noreferrer">DONATE</a>
+          </Link>
+        </div>
+      </div >
+      <Footer />
+    </>
+    // <div className="container">
+    //   <Head>
+    //     <title>pcos answered</title>
+    //     <meta name="description" content="spreading awareness about pcos!" />
+    //   </Head>
+    //   </div>
+
+    //   <Header />
+    //   <main>
+    //     <Link href="/landing">
+    //       <a className="styled-button">efefwe</a>
+    //     </Link>
+    //   </main>
+    // </div>
   );
 }
 
